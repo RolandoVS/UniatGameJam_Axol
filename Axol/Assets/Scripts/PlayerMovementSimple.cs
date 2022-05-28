7ee200693a533ebca51fcd7fa687808a34781e64
AlanFlores02 <yosefalanfloreslopez@outlook.com>
2022-05-28 16:56:46 -0700

    Códigos Control Cámara
    Y otras cosas

---
+++ Axol/Assets/Scripts/PlayerMovementSimple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementSimple : MonoBehaviour
+{
+    public float speed = 1f;
+    float hSpeed;
+    float vSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        hSpeed = Input.GetAxis("Horizontal") * speed;
+        vSpeed = Input.GetAxis("Vertical") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(hSpeed, 0, vSpeed);
+    }
+}
