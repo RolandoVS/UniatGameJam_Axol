7d4a7b8b01dd64bf3e51a202bfc4780f006f4b41
AlanFlores02 <yosefalanfloreslopez@outlook.com>
2022-05-28 15:40:49 -0700

    Walls

---
+++ Axol/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonnClick : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public Image credits;
+
+    public void ChangeScene(string scene)
+    {
+        Debug.Log(scene);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ToggleLevels()
+    {
+        if (levelHolder.activeSelf) { levelHolder.SetActive(false); }
+        else { levelHolder.SetActive(true); }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        credits.gameObject.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        credits.gameObject.SetActive(false);
+    }
+}
---
+++ Axol/Assets/Scripts/FadeAway.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAway : MonoBehaviour
+{
+    public float showTime = 5f;       // Velocidad de aparecer
+    public float fadeTime = 3f;    // Velocidad de desaparecer
+
+    public bool startAlpha = true;
+    public Image image;
+
+    bool end = false;
+    float alpha;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        if (!startAlpha) { image.canvasRenderer.SetAlpha(0); }
+        else { image.canvasRenderer.SetAlpha(0.99f); }
+    }
+
+    void Update()
+    {
+        alpha = image.canvasRenderer.GetAlpha();
+
+        if (!end) { image.CrossFadeAlpha(1.0f, showTime, false); end = true; }
+        else if (alpha == 1.0f) { image.CrossFadeAlpha(0f, fadeTime, false); }
+        else if (alpha == 0) { image.gameObject.SetActive(false); }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            image.gameObject.SetActive(false);
+        }
+    }
+}
